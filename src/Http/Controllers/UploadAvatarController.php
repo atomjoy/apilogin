16a65c65e51dd300471a5ebf29ea6502604d98ed
Atomjoy <atomjoy.official@gmail.com>
2023-08-05 16:55:19 +0200

    Update

--- src/Http/Controllers/UploadAvatarController.php
+++ src/Http/Controllers/UploadAvatarController.php
@@ -9,6 +9,7 @@ use Atomjoy\Apilogin\Events\UploadAvatar;
 use Atomjoy\Apilogin\Exceptions\JsonException;
 use Atomjoy\Apilogin\Http\Requests\UploadAvatarRequest;
 use Atomjoy\Apilogin\Models\Profile;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
@@ -55,6 +56,27 @@ class UploadAvatarController extends Controller
 		} catch (Exception $e) {
 			report($e);
 			throw new JsonException(__('apilogin.upload.avatar.error'), 422);
+		}
+	}
+
+	function remove(Request $request)
+	{
+		try {
+
+			$filename = 'avatars/' . Auth::id() . '.webp';
+
+			if (Storage::disk('public')->exists($filename)) {
+				Storage::disk('public')->delete($filename);
+				Auth::user()->profile()->update(['avatar' => null]);
+			}
+
+			return response()->json([
+				'message' => __('apilogin.remove.avatar.success'),
+			], 200);
+		} catch (Exception $e) {
+			report($e);
+			dd($e->getMessage());
+			throw new JsonException(__('apilogin.remove.avatar.error'), 422);
 		}
 	}
 }
